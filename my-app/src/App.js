89ba458a52d2eaf7388b54181cecff6339cbb777
Jawahir <jewellry7134@gmail.com>
2025-05-24 20:50:33 +0100

    Completed challenge 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
 import css from './App.module.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
+      <header className={css.AppHeader}>
+        <h1>My React App</h1>
+        <p>This is a simple React application.</p>
+        
+      </header>
+      <Sidebar />
     </div>
   );
 }
---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2>Sidebar</h2>
+      <a href="#.">My photos</a>
+      <a href="#.">My Illustrations</a>
+    <a href="#.">My Paintigs</a>
+    </div>
+  );
+}
+
+export default Sidebar;
